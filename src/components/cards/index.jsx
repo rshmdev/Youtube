d760a6d3379d24d50573fb7acdabd284289200c9
tiozinfk <fortpcnite21@gmail.com>
2022-12-25 22:21:09 -0300

    feat: card file create

---
+++ src/components/cards/index.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { AiFillCheckCircle } from "react-icons/ai";
+import "./style.css";
+
+export const Cards = ({ thumb, title, channel_name, id }) => {
+  const api_key = "AIzaSyBHWijsHOKic7SKMRPNiAbK6yp2Oshljtc";
+
+  const [views, setViews] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=${id}&key=${api_key}`
+      )
+      .then((res) => {
+        setViews(res.data.items);
+      });
+  }, []);
+
+  console.log(views);
+
+  return (
+    <div className="card-related">
+      <div className="div-thumb">
+        <img src={thumb} width="100%" height="100%" className="thumbnail" />
+      </div>
+      <div className="div-texts">
+        <div className="title">
+          <h3 className="p-title">{title}</h3>
+        </div>
+        <div className="channelTitle">
+          <p className="p-channelTitle">{channel_name}</p>
+          <AiFillCheckCircle size={13} />
+        </div>
+
+        {views.map((view) => {
+          return <p className="p-views">{view.statistics.viewCount} views</p>;
+        })}
+      </div>
+    </div>
+  );
+};
