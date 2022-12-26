7ec42c33e692aa6ad445eb666ecf0a53ad05e407
tiozinfk <fortpcnite21@gmail.com>
2022-12-25 23:39:18 -0300

    feat: id state created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import { ColumnVideos } from "./components/columnVideos";
+import { Header } from "./components/header";
 import { Player } from "./components/player";
 
 function App() {
+  const [id, setId] = useState("");
+
   return (
-    <div className="App">
-      <Player />
-      <ColumnVideos />
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <Player />
+        <ColumnVideos />
+      </div>
+    </>
   );
 }
 
