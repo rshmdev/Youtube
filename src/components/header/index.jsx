13542e7f6437dbd7ae7022171ac596b863a11a47
tiozinfk <fortpcnite21@gmail.com>
2022-12-25 23:38:49 -0300

    fix: header component refactor

---
+++ src/components/header/index.jsx
@@ -0,0 +1,37 @@
+import logo from "../../assets/logo.png";
+import user from "../../assets/user.png";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineSearch, AiOutlineVideoCameraAdd } from "react-icons/ai";
+import { IoNotificationsOutline } from "react-icons/io5";
+import "./style.css";
+
+export const Header = ({ id, setId }) => {
+  return (
+    <header>
+      <div className="header-container">
+        <nav className="nav-logo">
+          <RxHamburgerMenu size={21} />
+          <img width={90} src={logo} alt="youtube-logo" />
+        </nav>
+
+        <div className="div-input">
+          <input
+            className="input-search"
+            type="text"
+            name="search"
+            id="input-search"
+          />
+          <div className="search-button">
+            <AiOutlineSearch size={22} />
+          </div>
+        </div>
+
+        <nav className="nav-user">
+          <AiOutlineVideoCameraAdd size={21} />
+          <IoNotificationsOutline size={21} />
+          <img src={user} alt="user-logo" width={30} />
+        </nav>
+      </div>
+    </header>
+  );
+};
